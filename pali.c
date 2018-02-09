f9cedb3f984a4145f24e0e0fe8c14cbec7f62f6a
poovarasanmithran <31285551+poovarasanmithran@users.noreply.github.com>
2018-02-09 15:50:01 +0530

    pali.c

--- pali.c
+++ pali.c
@@ -1,12 +1,12 @@
 b=input()
 def pal(x):
-	a=x[::-1]
+	a=x[:-1]
 	return a
 x=pal(b)
 if b.isalpha():
 	if(x==b):
-		print("YES a Palindrome")
+		print("YES a Palindrome\n")
 	else:
-		print("Not a palindrome")
+		print("Not a palindrome\n")
 else:
 	print("INVALID INPUT")
